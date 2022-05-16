327b9f71200d2dc3bd8921415cfe18c8e9c84ffc
printerscanner <abbeyyacoe@gmail.com>
2022-05-16 11:20:01 +0100

    auto commit

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 date >> data.txt
 git add .
 git commit -m "auto commit"
-git remote set-url [--push] git@github.com:printerscanner/hack-my-contributions-golf.git
+git remote set-url origin git@github.com:printerscanner/hack-my-contributions-golf.git
 git push
